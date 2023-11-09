1d1caed72ad4086bf8e3623f2533bc3e6ece738c
Won A Young <zeronediary@naver.com>
2023-11-09 16:40:24 +0900

    게시물 삭제 기능

--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -40,4 +40,8 @@ public class BoardService {
     public Board get(Integer id) {
         return mapper.selectById(id);
     }
+
+    public boolean remove(Integer id) {
+        return mapper.deleteById(id)==1;
+    }
 }
