9c71de13e455ab39bc0e3d08e3041c905a58b593
Won A Young <zeronediary@naver.com>
2023-11-14 15:45:10 +0900

    권한 기능
    CREATE TABLE auth (
        id INT PRIMARY KEY AUTO_INCREMENT,
        memberId VARCHAR(100) NOT NULL REFERENCES member(id),
        name VARCHAR(100) NOT NULL
    );

--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.prj1be.service;
 
+import com.example.prj1be.domain.Auth;
 import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.BoardMapper;
 import com.example.prj1be.mapper.MemberMapper;
@@ -77,8 +78,11 @@ public class MemberService {
     public boolean login(Member member, WebRequest request) {
         Member dbMember = mapper.selectById(member.getId());
 
+
         if(dbMember != null){
             if(dbMember.getPassword().equals(member.getPassword())){
+                List<Auth> auth = mapper.selectAuthById(member.getId());//여러 권한을 가질 수 있으므로 list
+                dbMember.setAuth(auth);
                 dbMember.setPassword("");
                 request.setAttribute("login", dbMember, RequestAttributes.SCOPE_SESSION);
                 return true;
