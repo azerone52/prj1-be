89805456a8376899c2cca3b7eff5947a610c542a
Won A Young <zeronediary@naver.com>
2023-11-16 17:40:55 +0900

    좋아요 기능

--- src/main/java/com/example/prj1be/controller/LikeController.java
+++ src/main/java/com/example/prj1be/controller/LikeController.java
@@ -1,12 +1,12 @@
 package com.example.prj1be.controller;
 
 import com.example.prj1be.domain.Like;
+import com.example.prj1be.domain.Member;
 import com.example.prj1be.service.LikeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +15,16 @@ public class LikeController {
     private final LikeService service;
 
     @PostMapping
-    public void like(@RequestBody Like like){
-        service.update(like);
+    public ResponseEntity like(@RequestBody Like like,
+                               @SessionAttribute(value = "login", required = false) Member login) {
+        if (login == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        } else {
+            like.setMemberId(login.getId());
+        }
+        if (service.update(like)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.internalServerError().build();
     }
 }
--- src/main/java/com/example/prj1be/service/LikeService.java
+++ src/main/java/com/example/prj1be/service/LikeService.java
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeMapper mapper;
-    public void update(Like like) {
+    public boolean update(Like like) {
         // 처음 좋아요 누를 때: insert
         // 다시 누르면: delete
         int count = 0;
         if(mapper.delete(like) == 0){
             count = mapper.insert(like);
         }
+        return count==1;
     }
 }
