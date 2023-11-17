5d6c4d8e9a95c81f001307db922046bdeb529f8e
Won A Young <zeronediary@naver.com>
2023-11-17 09:23:14 +0900

    좋아요 응답 {like, countLike} 기능

--- src/main/java/com/example/prj1be/controller/LikeController.java
+++ src/main/java/com/example/prj1be/controller/LikeController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/Like")
@@ -15,16 +17,12 @@ public class LikeController {
     private final LikeService service;
 
     @PostMapping
-    public ResponseEntity like(@RequestBody Like like,
-                               @SessionAttribute(value = "login", required = false) Member login) {
+    public ResponseEntity<Map<String, Object>> like(@RequestBody Like like,
+                                                    @SessionAttribute(value = "login", required = false) Member login) {
         if (login == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        } else {
-            like.setMemberId(login.getId());
         }
-        if (service.update(like)) {
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.internalServerError().build();
+
+        return ResponseEntity.ok(service.update(like, login));
     }
 }
--- src/main/java/com/example/prj1be/service/LikeService.java
+++ src/main/java/com/example/prj1be/service/LikeService.java
@@ -1,21 +1,34 @@
 package com.example.prj1be.service;
 
 import com.example.prj1be.domain.Like;
+import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeMapper mapper;
-    public boolean update(Like like) {
-        // 처음 좋아요 누를 때: insert
-        // 다시 누르면: delete
+    public Map<String, Object> update(Like like, Member login) {
+
+        like.setMemberId(login.getId());
+
+        // 처음 좋아요 누를 때 : insert
+        // 다시 누르면 : delete
+
         int count = 0;
-        if(mapper.delete(like) == 0){
+        if (mapper.delete(like) == 0) {
             count = mapper.insert(like);
         }
-        return count==1;
+
+        int countLike = mapper.countByBoardId(like.getBoardId());
+
+        return Map.of("like", count == 1,
+                "countLike", countLike);
+
+
     }
 }
